c39a81429a09fb19a1bd7354425075ffd500ef2b
adiawal <ad_90ag@hotmail.com>
2018-10-12 20:32:48 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -vx
 cd ${WORKSPACE}  && sftp ${USERNAME}@corpdmz.staples.com:/files/continuous-deploy-test/2018_10_08_cd_test.zip .
-#curl -u ${ARTI_USERNAME}:${ARTI_PASSWORD}  -X PUT \"http://zrepo.staples.com/artifactory/craftworks-artifacts-local/2018_10_08_cd_test.zip\" ${WORKSPACE}/2018_10_08_cd_test.zip"
+curl -u ${ARTI_USERNAME}:${ARTI_PASSWORD}  -X PUT \"http://zrepo.staples.com/artifactory/craftworks-artifacts-local/2018_10_08_cd_test.zip\" ${WORKSPACE}/2018_10_08_cd_test.zip"
 
