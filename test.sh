514011a0f1ad03eacb55634c91d64e87a70341eb
root <root@localhost.localdomain>
2018-10-12 10:57:12 -0400

    first commit

---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ${WORKSPACE}  && sftp ${USERNAME}@corpdmz.staples.com:/files/continuous-deploy-test/2018_10_08_cd_test.zip .
+#curl -u ${ARTI_USERNAME}:${ARTI_PASSWORD}  -X PUT \"http://zrepo.staples.com/artifactory/craftworks-artifacts-local/2018_10_08_cd_test.zip\" ${WORKSPACE}/2018_10_08_cd_test.zip"
+
